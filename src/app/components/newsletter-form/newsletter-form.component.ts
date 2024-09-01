dc9583665ea1988b65d233662db477f65a1d74fc
Cicera Ribeiro <maccicera@gmail.com>
2024-09-01 17:42:53 -0300

    feat: Landing Page com Angula

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, signal } from '@angular/core';
+import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { NewsletterService } from '../../services/newsletter.service';
+
+@Component({
+  selector: 'newsletter-form',
+  standalone: true,
+  imports: [
+    BtnPrimaryComponent,
+    ReactiveFormsModule
+  ],
+  providers: [NewsletterService],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss'
+})
+export class NewsletterFormComponent {
+  newsletterForms!: FormGroup;
+  loading = signal(false);
+
+  constructor(private service: NewsletterService) {
+    this.newsletterForms = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
+  }
+
+  onSubmit(){
+    this.loading.set(true)
+    if(this.newsletterForms.valid){
+      this.service.sendData(this.newsletterForms.value.name, this.newsletterForms.value.email).subscribe({
+        next:() =>{
+          this.newsletterForms.reset();
+          this.loading.set(false);
+        }
+      })
+    }
+  }
+}
